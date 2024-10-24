dcbbfb84bfc1af7c84d016bc400df55bd16d15f6
giorgimaisuradze21 <maisuradzegio40@gmail.com>
2024-10-24 20:11:21 +0400

    first commit

---
+++ Level47/Homework/main.py
@@ -0,0 +1,90 @@
+# # შექმენი ფუნქცია, რომელიც მიიღებს ტექსტს და გამოითვლის, რამდენი სიტყვაა ამ ტექსტში.
+# def function(text):
+    # return len(text)?????
+  
+
+
+
+# # შექმენი პროგრამა, რომელიც იფუნქციონირებს შემდეგნაირად: მომხმარებლის შეყვანილი რიცხვი იქნება დადებითი, უარყოფითი, ან ნულოვანი, და შესაბამისი შეტყობინება უნდა გამოიტანოს.
+# def check_number():
+#     try:
+#         number = float(input("Enter a number: ")
+
+#         # Check if the number is positive, negative, or zero
+#         if number > 0:
+#             print("The number is positive.")
+#         elif number < 0:
+#             print("The number is negative.")
+#         else:
+#             print("The number is zero.")
+#     except ValueError:
+#         print("Please enter a valid number.")
+# check_number()
+
+
+
+#  შექმენი პროგრამა, რომელიც მიიღებს მომხმარებლის ასაკს და დააბრუნებს შეტყობინებას იმის მიხედვით, თუ რომელ ასაკობრივ კატეგორიაშია მომხმარებელი
+
+# def check_age(age):
+#     if age >= 18:
+#         return "srulwlovanixar"
+#     elif age <=18:
+#         return "arasrulwlovanixar"
+        
+#     elif age <=16:
+#         return "arcisepataraxar"
+#     elif age <=13:
+#         return "cotapataraxar"
+#     else:
+#         return "bavshvixar"
+
+# user_age = int(input("Enter your number: "))
+# category = check_age(user_age)
+# print(f"uewveli{category}")
+
+
+
+
+
+# შექმენი ფუნქცია, რომელიც მიიღებს რიცხვების სიას და დააბრუნებს ორ ცალკე სიას – ერთში იქნებიან ლუწი რიცხვები, ხოლო მეორეში კენტი რიცხვები.
+# def even_odd(numbers):
+#     even_numbers = []
+#     odd_numbers = []
+    
+#     for number in numbers:
+#         if number % 2 == 0:
+#             even_numbers.append(number)
+#         else:
+#             odd_numbers.append(number)
+    
+#     return even_numbers, odd_numbers
+
+# numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# even, odd = even_odd(numbers_list)
+# print("even numbers:", even)
+# print("odd numbers:", odd)
+
+
+# შექმენი ფუნქცია, რომელიც მიიღებს რიცხვების სიას და დააბრუნებს სიის საშუალო რიცხვს. 
+
+
+# def average_numbers(numbers):
+#     if not numbers:
+#         return 0
+    
+#     total = sum(numbers)
+#     average = total / len(numbers)
+#     return average
+
+# # მაგალითი
+# numbers_list = [10, 20, 30, 40, 50]
+# average = average_numbers(numbers_list)
+
+# print("average_numbers:", average)
+    
+
+
+
+
+print("50+40+30+20+10")
+
